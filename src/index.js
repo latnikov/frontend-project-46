3267dccb2c758a5e87fb7cf2d042b00449b341d2
Latnikov <latnikou@gmail.com>
2024-05-23 15:21:50 +0300

    prestep 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import { parse } from './parser.js';
+import _ from 'lodash';
 
 const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
@@ -12,10 +13,21 @@ export const genDiff = (path1, path2) => {
   const data1 = readFileSync(fullPath1, 'utf-8')
   const data2 = readFileSync(fullPath2, 'utf-8')
   const [ parsedObj1, parsedObj2 ] = parse(data1, data2);
-  
-  console.log(parsedObj1, parsedObj2);
-  
 
+  // найти пересечение при помощи _.union
+  console.log(parsedObj1);
+  const keys1 = Object.keys(parsedObj1);
+  const keys2 = Object.keys(parsedObj2);
+  const sortedKeys = _.union(keys1, keys2).sort();
+  console.log(sortedKeys);
+
+  const diff = sortedKeys.reduce((acc, key) => {
+    if (Object.hasOwn(parsedObj1, key) && !Object.hasOwn(parsedObj2, key)) {
+      acc.push(` - ${key}:${parsedObj1[key]}`)
+    }
+    return acc;
+  }, ['{'])
+  console.log(diff);
 }
 
 export default genDiff;
